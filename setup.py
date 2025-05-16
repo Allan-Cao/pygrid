7616095ece5bccfa6c6150b9067d00ff75cd5d34
Allan Cao <allan@allancao.ca>
2025-05-15 18:53:21 -0600

    fix: Add pydantic to automatically installed libraries to fix dependency issue

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="pythongrid",
-    version="0.1.1.0",
+    version="0.1.1.1",
     packages=find_packages(),
     install_requires=[
         "httpx",
         "orjson",
         "arrow",
+        "pydantic",
     ],
     author="Allan Cao",
     author_email="allan@allancao.ca",
